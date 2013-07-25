117f09f31ee0e9c271021895fa02506b670cf5cd
Hyounggyu Kim <khg@gist.ac.kr>
2013-07-25 17:19:24 +0900

    Add __init__.py to make a python package

---
+++ xni/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    xni
+    ~~~
+
+    :copyright: (c) 2013 by Hyounggyu Kim.
+    :license: GPL, see LICENSE for more details.
+"""
+
+__version__ = '0.0.1'
+
+from .config import ConfigWindow
+from .shift import ShiftImage

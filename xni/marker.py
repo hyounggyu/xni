22d7576b282d56823961013425dcc117d8c93267
Hyounggyu Kim <khg@gist.ac.kr>
2013-11-04 22:34:38 +0900

    Add intensity sliders

--- xni/marker.py
+++ xni/marker.py
@@ -10,38 +10,89 @@
 import sys, os, json
 from functools import partial
 
+import numpy as np
+
+import matplotlib
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4']='PySide'
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+from skimage import data, transform, util
+
 from PySide import QtGui, QtCore
 
 class MainWindow(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        self.img = util.img_as_float(data.imread('sample.tif'))
+        self.orig_img = self.img.copy()
+        # is 8 bit or 16 bit?
+        self.imax = int(self.img.max()*50)/50. # 0.02 씩 
+        self.imin = int(self.img.min()*50)/50. # 0.02 씩
+        self.scale = 1.
+        self.width, self.height = self.img.shape
         self.initUI()
-        self.load()
 
     def initUI(self):
-    	self.imageLabel = QtGui.QLabel()
-    	self.imageLabel.setBackgroundRole(QtGui.QPalette.Base)
-    	self.imageLabel.setSizePolicy(QtGui.QSizePolicy.Ignored,
-    		QtGui.QSizePolicy.Ignored)
-    	self.imageLabel.setScaledContents(True)
+        self.fig = plt.figure()
+        canvas = FigureCanvas(self.fig)
+        canvas.setFixedSize(700,700)
 
-    	scrollArea = QtGui.QScrollArea()
-    	scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
-    	scrollArea.setWidget(self.imageLabel)
-    	self.setCentralWidget(scrollArea)
+        iminEdit = QtGui.QLineEdit(str(self.imin))
+        imaxEdit = QtGui.QLineEdit(str(self.imax))
 
-    	self.resize(500,400)
+        iminSld = QtGui.QSlider(self)
+        iminSld.setOrientation(QtCore.Qt.Horizontal)
+        iminSld.setRange(0, 50) # test range
+        iminSld.setSliderPosition(int(self.imin*50)) # float to int
+        iminSld.valueChanged[int].connect(partial(self.updateIntensity, 'IMIN', iminEdit))
+
+        imaxSld = QtGui.QSlider(self)
+        imaxSld.setOrientation(QtCore.Qt.Horizontal)
+        imaxSld.setRange(0, 50) # test range
+        imaxSld.setSliderPosition(int(self.imax*50)) # float to int
+        imaxSld.valueChanged[int].connect(partial(self.updateIntensity, 'IMAX', imaxEdit))
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(iminEdit)
+        vbox.addWidget(iminSld)
+        vbox.addWidget(imaxEdit)
+        vbox.addWidget(imaxSld)
+
+        centralWidget = QtGui.QWidget(self)
+        hbox = QtGui.QHBoxLayout(centralWidget)
+        hbox.addWidget(canvas)
+        hbox.addLayout(vbox)
+        self.setCentralWidget(centralWidget)
+
+        self.resize(1000,800)
+
+        self.updateView()
+
+    def updateIntensity(self, tag, widget, value):
+        value = value / 50. # test range
+        if tag == 'IMAX':
+            self.imax = value
+        elif tag == 'IMIN':
+            self.imin = value
+        widget.setText(str(value))
+        self.updateView()
+
+    def updateScale(self):
+        self.img = transform.rescale(self.orig_img, self.scale)
+        updateView()
+
+    def updateView(self):
+        self.fig.figimage(self.img, cmap=plt.gray(), imin=self.imin, imax=self.imax)
+        self.fig.canvas.draw()
 
     def msgBox(self, msg):
         msgbox = QtGui.QMessageBox()
         msgbox.setText(msg)
         msgbox.exec_()
-
-    def load(self):
-    	self.imageLabel.setPixmap(QtGui.QPixmap.fromImage(QtGui.QImage('sample.tif')))
-    	self.imageLabel.adjustSize()
-    	self.scaleFactor = 1.0
 
 class App(QtGui.QApplication):
     def __init__(self, *argv):

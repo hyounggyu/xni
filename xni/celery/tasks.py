3219235f0d3133846007c5ac54bc168e30561000
Hyounggyu Kim <hgkim10@gmail.com>
2014-07-01 23:53:45 +0900

    Test a database task

--- xni/celery/tasks.py
+++ xni/celery/tasks.py
@@ -1,7 +1,23 @@
-from __future__ import absolute_import
+from celery import Task
+import redis
 
 from .celery import app
+
+class DatabaseTask(Task):
+    abstract = True
+    _db = None
+
+    @property
+    def db(self):
+        if self._db is None:
+            self._db = redis.StrictRedis(db=1) # add database information
+        return self._db
 
 @app.task
 def add(x, y):
     return x + y
+
+@app.task(base=DatabaseTask)
+def read():
+    read.db.set('a','b')
+    return read.db.get('a')

6a671a10c9aafe96b54015bff96e455e84287434
Hyounggyu Kim <khg@gist.ac.kr>
2014-07-07 13:38:40 +0900

    Test vertical alignments

--- xni/celery/tasks.py
+++ xni/celery/tasks.py
@@ -1,17 +1,8 @@
 from celery import Task
-import redis
 
 from .celery import app
-
-class DatabaseTask(Task):
-    abstract = True
-    _db = None
-
-    @property
-    def db(self):
-        if self._db is None:
-            self._db = redis.StrictRedis(db=1) # add database information
-        return self._db
+from .databasetask import DatabaseTask
+from ..preprocess.shift import sum_x, vertical_align
 
 @app.task
 def add(x, y):
@@ -21,3 +12,11 @@ def add(x, y):
 def read():
     read.db.set('a','b')
     return read.db.get('a')
+
+@app.task(base=DatabaseTask)
+def task_vertical_align(y0, im, start, end, dy_max):
+    y0 = task_vertical_align.fetch(y0)
+    im = task_vertical_align.fetch(im)
+    y = sum_x(im)
+    dy = vertical_align(y0, y, start, end, dy_max)
+    return dy
--- xni/preprocess/shift.py
+++ xni/preprocess/shift.py
@@ -7,8 +7,20 @@
     :license: GPL, see LICENSE for more details.
 """
 
+import numpy as np
 from scipy.ndimage.interpolation import shift
 
 
 def shift_image(im, dx, dy):
-    return shift(im, (dy, dy))
+    return shift(im, (dy, dx))
+
+def sum_x(im):
+    return np.sum(im, axis=1)
+
+def vertical_align(im0_y, im_y, start, end, dy_max):
+    """
+    dy_max: -dy_max <= dy <= dy_max
+    """
+    corr = np.correlate(im_y[start-dy_max:end+dy_max], im0_y[start:end])
+    dy = dy_max - np.argmax(corr)
+    return dy

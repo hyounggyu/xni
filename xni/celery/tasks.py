39d4b65f81166bb5f7d0fb0a70e20b3f881da88d
Hyounggyu Kim <khg@gist.ac.kr>
2014-07-07 22:26:51 +0900

    Add vertical_align2 task

--- xni/celery/tasks.py
+++ xni/celery/tasks.py
@@ -20,3 +20,12 @@ def task_vertical_align(y0, im, start, end, dy_max):
     y = sum_x(im)
     dy = vertical_align(y0, y, start, end, dy_max)
     return dy
+
+@app.task(base=DatabaseTask)
+def task_vertical_align2(im0, im, start, end, dy_max):
+    im0 = task_vertical_align.fetch(im0)
+    im = task_vertical_align.fetch(im)
+    y0 = sum_x(im0)
+    y = sum_x(im)
+    dy = vertical_align(y0, y, start, end, dy_max)
+    return dy

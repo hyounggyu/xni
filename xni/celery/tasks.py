b8c754bd9454eba8f6ed263f242f3aff535017b6
Hyounggyu Kim <khg@gist.ac.kr>
2014-07-08 15:21:46 +0900

    Add vertical_align2 function, remove tasks
    Celery tasks are too slow.

--- xni/celery/tasks.py
+++ xni/celery/tasks.py
@@ -2,7 +2,6 @@ from celery import Task
 
 from .celery import app
 from .databasetask import DatabaseTask
-from ..preprocess.shift import sum_x, vertical_align
 
 @app.task
 def add(x, y):
@@ -12,20 +11,3 @@ def add(x, y):
 def read():
     read.db.set('a','b')
     return read.db.get('a')
-
-@app.task(base=DatabaseTask)
-def task_vertical_align(y0, im, start, end, dy_max):
-    y0 = task_vertical_align.fetch(y0)
-    im = task_vertical_align.fetch(im)
-    y = sum_x(im)
-    dy = vertical_align(y0, y, start, end, dy_max)
-    return dy
-
-@app.task(base=DatabaseTask)
-def task_vertical_align2(im0, im, start, end, dy_max):
-    im0 = task_vertical_align.fetch(im0)
-    im = task_vertical_align.fetch(im)
-    y0 = sum_x(im0)
-    y = sum_x(im)
-    dy = vertical_align(y0, y, start, end, dy_max)
-    return dy
--- xni/preprocess/shift.py
+++ xni/preprocess/shift.py
@@ -14,13 +14,15 @@ from scipy.ndimage.interpolation import shift
 def shift_image(im, dx, dy):
     return shift(im, (dy, dx))
 
-def sum_x(im):
-    return np.sum(im, axis=1)
-
 def vertical_align(a, v, start, end, dy_max):
     """
     dy_max: -dy_max <= dy <= dy_max
     """
     corr = np.correlate(a[start-dy_max:end+dy_max], v[start:end])
     dy = np.argmax(corr) - dy_max
+    return dy
+
+def vertical_align2(a, v, start, end, dy_max):
+    sqsum = [np.sum(np.square(a[start:end] - v[start + i:end + i])) for i in range(-dy_max, dy_max+1)]
+    dy = np.argmin(sqsum) - dy_max
     return dy

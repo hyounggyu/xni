a3b55acf312e78b2647a6e56cd68e34cb8193445
Hyounggyu Kim <khg@gist.ac.kr>
2014-07-08 16:33:59 +0900

    Change nomalize function (add log)

--- xni/preprocess/normalize.py
+++ xni/preprocess/normalize.py
@@ -7,18 +7,8 @@
     :license: GPL, see LICENSE for more details.
 """
 
-import os
 import numpy as np
 
 
-class NormalizeImage:
-
-    def __init__(self, bgimg):
-        self.bgimg = bgimg.astype(np.float32, copy=False)
-        self.vsat = np.vectorize(self.saturate)
-
-    def norm(self, img):
-        return vsat(img / bg)
-
-    def saturate(self, pixel):
-        return 1. if pixel > 1. else pixel
+def normalize(bg, im):
+    return np.log(im / bg)

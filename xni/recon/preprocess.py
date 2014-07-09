6be8ecfe92a047cf6c2ed514d1273fcc819406f3
Hyounggyu Kim <khg@gist.ac.kr>
2014-07-09 16:45:31 +0900

    Change a file name

--- xni/recon/preprocess.py
+++ xni/recon/preprocess.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-    xni.recon.normalize
-    ~~~~~~~~~~~~~
+    xni.recon.preprocess
+    ~~~~~~~~~~~~~~~~~~~~
 
     :copyright: (c) 2014 by Hyounggyu Kim.
     :license: GPL, see LICENSE for more details.

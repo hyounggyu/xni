b4a960d3eb349d5a4e0d829dc258e93e9b0c3430
Hyounggyu Kim <hgkim10@gmail.com>
2014-08-22 14:43:44 +0900

    Combine tornado and zeromq and muliprocessing

--- xni/server/manager.py
+++ xni/server/manager.py
@@ -5,22 +5,17 @@ import zmq
 
 import tornado.web
 
+import worker
+
 class BaseHandler(tornado.web.RequestHandler):
     pass
 
 class MainHandler(BaseHandler):
     def get(self):
         self.write("Hello, world")
+        sender.send_string('Hello')
 
 def main():
-    context = zmq.Context()
-
-    sender = context.socket(zmq.PUSH)
-    print ('bind...')
-    sender.bind('tcp://127.0.0.1:9305')
-    sender.send_string('Hello')
-
-def webserver_main():
     app = tornado.web.Application(
         [
             (r'/', MainHandler),
@@ -30,4 +25,11 @@ def webserver_main():
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
+    global sender
+    context = zmq.Context()
+    sender = context.socket(zmq.PUSH)
+    sender.bind('tcp://127.0.0.1:9305')
+    p = Process(target=worker.start)
+    p.start()
     main()
+    p.join()
--- xni/server/worker.py
+++ xni/server/worker.py
@@ -1,10 +1,13 @@
 import zmq
 
-context = zmq.Context()
+def start():
+    context = zmq.Context()
+    receiver = context.socket(zmq.PULL)
+    receiver.connect('tcp://127.0.0.1:9305')
 
-receiver = context.socket(zmq.PULL)
-receiver.connect('tcp://127.0.0.1:9305')
+    while True:
+        s = receiver.recv_string()
+        print(s)
 
-while True:
-    s = receiver.recv_string()
-    print(s)
+if __name__ == '__main__':
+    start()

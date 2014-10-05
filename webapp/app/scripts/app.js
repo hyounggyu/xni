383b9a1b1bcc5c24f69a483e2c43e2432ab5c37f
Hyounggyu Kim <hgkim10@gmail.com>
2014-10-05 18:37:33 +0900

    Change js indentation

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -9,25 +9,25 @@
  * Main module of the application.
  */
 angular
-  .module('xniApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/align', {
-        templateUrl: 'views/align.html',
-        controller: 'AlignCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('xniApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/align', {
+                templateUrl: 'views/align.html',
+                controller: 'AlignCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });

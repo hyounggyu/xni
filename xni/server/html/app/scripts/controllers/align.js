4d0aaa335897dc50bbddda5518a07fce805eba24
Hyounggyu Kim <hgkim10@gmail.com>
2014-10-05 09:20:51 +0900

    Add AlignCtrl

---
+++ xni/server/html/app/scripts/controllers/align.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xniApp.controller:AlignCtrl
+ * @description
+ * # AlignCtrl
+ * Controller of the xniApp
+ */
+angular.module('xniApp')
+  .controller('AlignCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.imfiles_change = function() {
+        $http({method: 'POST', url: '/api/v1/path/', params: {
+            path: $scope.imfiles
+        }}).
+            success(function(data, status){
+                $scope.imfiles_status = data;
+            }).
+            error(function(data, status){
+                $scope.imfiles_status = data;
+            });
+    };
+    $scope.destdir_change = function() {
+        $http({method: 'POST', url: '/api/v1/path/', params: {
+            path: $scope.destdir
+        }}).
+            success(function(data, status){
+                $scope.destdir_status = data;
+            }).
+            error(function(data, status){
+                $scope.destdir_status = data;
+            });
+    };
+    $scope.shift = function() {
+        $http({method: 'POST', url: '/api/v1/shift/', params: {
+            imfiles: $scope.imfiles,
+            destdir: $scope.destdir,
+            posdata: $scope.posdata
+        }}).
+            success(function(data, status) {
+                $scope.response = data
+            }).
+            error(function(data, status) {
+                $scope.response = data
+            });
+    };
+
+  });

315bbad2354b7e8e5dfc5ba64296b43e6529437e
Hyounggyu Kim <hgkim10@gmail.com>
2014-10-10 21:15:01 +0900

    Update cross-correlation test function

--- xni/testing/ccorr2d.py
+++ xni/testing/ccorr2d.py
@@ -1,21 +1,51 @@
 import os, sys
 import numpy as np
+from scipy.ndimage.interpolation import shift
+import matplotlib.pyplot as plt
 
 sys.path.append(os.path.join('..','..'))
 
 from xni.align import correlation
 
+np.set_printoptions(precision=3)
+
 A = np.array([[1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1],
-              [1, 1, 1, 3, 3],
-              [1, 1, 1, 3, 3],
-              [1, 1, 1, 1, 1]])
+              [1, 3, 3, 3, 1],
+              [1, 3, 5, 3, 1],
+              [1, 3, 3, 3, 1],
+              [1, 1, 1, 1, 1]], dtype=np.double)
 
-B = np.array([[1, 1, 1, 1, 1],
-              [1, 3, 3, 1, 1],
-              [1, 3, 3, 1, 1],
-              [1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1]])
+A = np.pad(A, 10, mode='constant', constant_values=1)
 
-# expected (1,2)
-print (correlation.ccorr2d(A, B))
+err_x, err_y = 0, 0
+
+DEBUG = False
+SHOW = False
+
+for _ in range(1000):
+    rand_dy = np.ceil(np.random.random()*50.-100.)/10.
+    rand_dx = np.ceil(np.random.random()*50.-100.)/10.
+    B = shift(A, (rand_dy, rand_dx), mode='constant', cval=1.0)
+    ccorr_dy, ccorr_dx = correlation.ccorr2d(A, B)
+    if DEBUG == True:
+        print (A)
+        print (B)
+        print (rand_dy, rand_dx)
+        print (ccorr_dy, ccorr_dx)
+        print (np.fabs(rand_dy-ccorr_dy), np.fabs(rand_dx-ccorr_dx))
+    err_y = err_y + (0 if np.fabs(rand_dy-ccorr_dy) < 0.1 else 1)
+    err_x = err_x + (0 if np.fabs(rand_dx-ccorr_dx) < 0.1 else 1)
+
+print ('----')
+print (err_y, err_x)
+
+if SHOW == True:
+    fig = plt.figure(figsize=plt.figaspect(2.))
+    ax = fig.add_subplot(2, 1, 1)
+    ax.imshow(A)
+    ax = fig.add_subplot(2, 1, 2)
+    ax.imshow(B)
+    plt.show()
+
+# ValueError
+#print (correlation.ccorr2d(A, C))

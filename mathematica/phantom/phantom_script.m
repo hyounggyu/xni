bc6cc9575c38f03b5602d7acc6165dcb11277dcb
Hyounggyu Kim <hgkim10@gmail.com>
2015-03-01 21:57:20 +0900

    Update phantom generation script

--- mathematica/phantom/phantom_script.m
+++ mathematica/phantom/phantom_script.m
@@ -1,9 +1,5 @@
-
 <<Phantom3D`
 
 regions = Table[Phantom3DTransformedRegion[{RotationTransform[th, {0, 0, 1}]}], {th, 0, Pi, Pi/180}];
-Do[Print[i];
- data = Phantom3DRegionToImage3D[regions[[i]], 400]; 
- im = ImageAdd@Image3DSlices[data, All, 3]; 
- Export[FileNameJoin[{Environment["PWD"], "im"<>ToString@i<>".h5"}], ImageData@im];
- Print@MaxMemoryUsed[], {i, Length@regions}]
+im = ImageAdd@Image3DSlices[Phantom3DRegionToImage3D[#, 400], All, 3]& /@ regions 
+Export[FileNameJoin[{Environment["PWD"], "im.h5"}], ImageData/@im];

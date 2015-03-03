a15635ac269d5c07f9762360fbf7aa0285219525
Hyounggyu Kim <hgkim10@gmail.com>
2015-03-03 22:10:36 +0900

    Change ViewWindow(ImageView)

--- xni/ui/view.py
+++ xni/ui/view.py
@@ -14,7 +14,22 @@ class ViewWindow(QtGui.QMainWindow):
 
     def initUI(self):
         #centralWidget = QtGui.QWidget(self)
-        pw = pg.PlotWidget(name='Plot1')
-        pw.plot(np.random.normal(size=100), pen=(255,0,0))
+        imv = pg.ImageView()
+        img = pg.gaussianFilter(np.random.normal(size=(200, 200)), (5, 5)) * 20 + 100
+        img = img[np.newaxis,:,:]
+        decay = np.exp(-np.linspace(0,0.3,100))[:,np.newaxis,np.newaxis]
+        data = np.random.normal(size=(100, 200, 200))
+        data += img * decay
+        data += 2
 
-        self.setCentralWidget(pw)
+        sig = np.zeros(data.shape[0])
+        sig[30:] += np.exp(-np.linspace(1,10, 70))
+        sig[40:] += np.exp(-np.linspace(1,10, 60))
+        sig[70:] += np.exp(-np.linspace(1,10, 30))
+
+        sig = sig[:,np.newaxis,np.newaxis] * 3
+        data[:,50:60,50:60] += sig
+
+        imv.setImage(data, xvals=np.linspace(1., 3., data.shape[0]))
+
+        self.setCentralWidget(imv)

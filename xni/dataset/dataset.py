911ad8ec5d98c381100bdd99e75f8281bc5fc47c
Hyounggyu Kim <hgkim10@gmail.com>
2015-03-06 15:29:25 +0900

    Update view window

--- xni/dataset/dataset.py
+++ xni/dataset/dataset.py
@@ -1,4 +1,12 @@
+import os
+import h5py
+
+PATH='/Users/hgkim/Data/workspaces/XrayNanoImaging/ipython/sample'
+
 class Dataset:
 
     def __init__(self):
-        pass
+        # self.hf = h5py.File(os.path.join(PATH,'tm181x400x400.h5'),'r')
+        # self.origin = self.hf['original'][:]
+        self.hf = h5py.File(os.path.join(PATH,'phantom3d.h5'),'r')
+        self.origin = self.hf['Dataset1'][:]
--- xni/ui/view.py
+++ xni/ui/view.py
@@ -1,36 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 import pyqtgraph as pg
 
 import numpy as np
+
+from ..dataset.dataset import Dataset
 
 
 class ViewWindow(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         super(ViewWindow, self).__init__(parent)
+        self.data = Dataset()
         self.initUI()
 
     def initUI(self):
-        #centralWidget = QtGui.QWidget(self)
+        # centralWidget = QtGui.QWidget(self)
         imv = pg.ImageView()
-        img = pg.gaussianFilter(np.random.normal(size=(200, 200)), (5, 5)) * 20 + 100
-        img = img[np.newaxis,:,:]
-        decay = np.exp(-np.linspace(0,0.3,100))[:,np.newaxis,np.newaxis]
-        data = np.random.normal(size=(100, 200, 200))
-        data += img * decay
-        data += 2
-
-        sig = np.zeros(data.shape[0])
-        sig[30:] += np.exp(-np.linspace(1,10, 70))
-        sig[40:] += np.exp(-np.linspace(1,10, 60))
-        sig[70:] += np.exp(-np.linspace(1,10, 30))
-
-        sig = sig[:,np.newaxis,np.newaxis] * 3
-        data[:,50:60,50:60] += sig
-
-        imv.setImage(data, xvals=np.linspace(1., 3., data.shape[0]))
-
+        imv.setImage(np.swapaxes(self.data.origin*10., 1, 2))
         self.setCentralWidget(imv)
+
+        # win = pg.GraphicsLayoutWidget()
+        # view = win.addViewBox()
+        # view.setAspectLocked(True)
+        # img = pg.ImageItem(border='w')
+        # view.addItem(img)
+        # view.setRange(QtCore.QRectF(0, 0, 600, 600))
+        # img.setImage(np.swapaxes(self.data.origin[0],0,1))
+        # self.setCentralWidget(win)

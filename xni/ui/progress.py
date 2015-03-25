21d07a26224c3de570bffb593db7312122411d4e
Hyounggyu Kim <hgkim10@gmail.com>
2015-03-25 17:44:36 +0900

    Add progress window ui file

--- xni/ui/progress.py
+++ xni/ui/progress.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtGui, QtCore
+import os
+
+from PyQt4 import QtGui, QtCore, uic
 
 
 class progressWindow(QtGui.QMainWindow):
+
+    step = 0
 
     def __init__(self, async_result, parent=None):
         super(progressWindow, self).__init__(parent)
@@ -13,42 +17,27 @@ class progressWindow(QtGui.QMainWindow):
         self.initUI()
 
     def initUI(self):
+        uic.loadUi(os.path.join('xni', 'ui', 'progresswindow.ui'), self)
 
-        self.pbar = QtGui.QProgressBar(self)
-        self.pbar.setGeometry(30, 40, 200, 25)
-
-        self.btn = QtGui.QPushButton('Stop', self)
-        self.btn.move(40, 80)
-        self.btn.clicked.connect(self.stop)
+        self.stopButton.clicked.connect(self.stop)
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.timerEvent)
-        self.step = 0
-
-        self.setGeometry(300, 300, 280, 170)
-        self.setWindowTitle('QtGui.QProgressBar')
-
         self.timer.start(1000)
         self.parent.setEnabled(False)
         self.setEnabled(True)
         self.show()
 
     def timerEvent(self):
-
-        print(self.ar.progress)
-
         if self.step >= 100.0:
-
             self.timer.stop()
             self.parent.setEnabled(True)
             self.close()
             return
-
         self.step = 100.0 * self.ar.progress / len(self.ar)
-        self.pbar.setValue(self.step)
+        self.progressBar.setValue(self.step)
 
     def stop(self):
-
         self.timer.stop()
         self.ar.abort()
         self.parent.setEnabled(True)

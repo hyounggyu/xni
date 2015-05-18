be508a3c5892812598774e6c99e2c7720a581f52
Hyounggyu Kim <hgkim10@gmail.com>
2015-05-18 21:28:30 +0900

    Remove mathematica project

--- mathematica/phantom/phantom_script.m
+++
@@ -1,5 +0,0 @@
-<<Phantom3D`
-
-regions = Table[Phantom3DTransformedRegion[{RotationTransform[th, {0, 0, 1}]}], {th, 0, Pi, Pi/180}];
-im = ImageAdd@Image3DSlices[Phantom3DRegionToImage3D[#, 400], All, 3]& /@ regions 
-Export[FileNameJoin[{Environment["PWD"], "im.h5"}], ImageData/@im];

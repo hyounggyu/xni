fdea92fa9a29cac65789e0e8409a331f6c05a9ed
Hyounggyu Kim <hgkim10@gmail.com>
2015-05-21 11:26:41 +0900

    Sperate projects

--- matlab/powerspectrum/exportdata.m
+++
@@ -1,7 +0,0 @@
-% Usage:
-%  exportdata('powerspecdata.txt', freq, power)
-
-function exportdata(filename, freq, power)
-  powerspecdata = transpose([freq; power]);
-  save(filename, 'powerspecdata', '-ascii');
-end
--- matlab/powerspectrum/loglogplot.m
+++
@@ -1,9 +0,0 @@
-% Usage:
-%  loglogplot(freq, power, 10, 1000)
-
-function loglogplot(freq, power, min_r, max_r)
-  loglog(freq(min_r:max_r)/10^6, power(min_r:max_r)); % '10^6' => 1/um
-  set(gca, 'XTick', [0.1 0.2 0.4 0.6 1 2 4 6 8 10 20 40 60 ]);
-  xlabel('Spatial Frequency [ 1/um ]')
-  ylabel('Power (Arbitary Units)')
-end

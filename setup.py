e22675e6d5c811cae8a26f3975fd7a3fc091d77f
Hyounggyu Kim <hgkim10@gmail.com>
2015-05-29 17:02:35 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='xni',
+    version='0.5.0',
+    description='A Software for X-Ray Nano Imaging Beamline in Pohang Accelerator Laboratory',
+    author='Hyounggyu Kim',
+    author_email='hgkim10@gmail.com',
+    url='https://github.com/hyounggyu/xni',
+    packages=find_packages()
+    )

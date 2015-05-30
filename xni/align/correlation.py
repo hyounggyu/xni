8c0ff513aee2750419bb9aab49dd1a706b2cddd3
Hyounggyu Kim <hgkim10@gmail.com>
2015-05-30 18:54:23 +0900

    Change function name

--- xni/align/correlation.py
+++ xni/align/correlation.py
@@ -2,16 +2,16 @@ import numpy as np
 from scipy import linalg
 
 
-__all__ = ['ccorr2d']
+__all__ = ['corr2d']
 
 PARABOLOID = None
 
 
-def limit_abs_one(val):
+def _limit_abs_one(val):
     return val if np.fabs(val) < 1. else 1.*np.sign(val)
 
 
-def init_paraboloid():
+def _init_paraboloid():
     global PARABOLOID
     PARABOLOID = np.zeros((9, 6))
     for i in range(9):
@@ -21,7 +21,7 @@ def init_paraboloid():
         PARABOLOID[i,:] = ix*ix, iy*iy, ix*iy, ix, iy, 1
 
 
-def ccorr2d(ref, trans):
+def corr2d(ref, trans):
     '''2D Cross-correlation with Paraboloid Fitting.
 
     References:
@@ -31,7 +31,7 @@ def ccorr2d(ref, trans):
     global PARABOLOID
 
     if PARABOLOID == None:
-        init_paraboloid()
+        _init_paraboloid()
 
     if np.shape(ref) != np.shape(trans):
         raise ValueError('Arrays shape should be same.')
@@ -39,12 +39,12 @@ def ccorr2d(ref, trans):
     ny, nx = np.shape(ref)
 
     # 2D Cross-correlation
-    ccorr = np.fft.ifft2(np.multiply(np.conj(np.fft.fft2(ref)), np.fft.fft2(trans)))
-    peak_y, peak_x = np.unravel_index(np.argmax(ccorr), ccorr.shape)
+    corr = np.fft.ifft2(np.multiply(np.conj(np.fft.fft2(ref)), np.fft.fft2(trans)))
+    peak_y, peak_x = np.unravel_index(np.argmax(corr), corr.shape)
 
     # Reference: Stack Overflow: 4148292
     # Given a 2D-array, returns an 3x3 array whose "center" element is arr[y,x]
-    arr = np.real(np.roll(np.roll(ccorr, shift=-peak_y+1, axis=0), shift=-peak_x+1, axis=1)[:3,:3])
+    arr = np.real(np.roll(np.roll(corr, shift=-peak_y+1, axis=0), shift=-peak_x+1, axis=1)[:3,:3])
 
     # Least square fit
     c, resid, rank, sigma = linalg.lstsq(PARABOLOID, arr.flatten())
@@ -53,8 +53,8 @@ def ccorr2d(ref, trans):
     ysh, xsh = 0.0, 0.0
     denom = 4*c[0]*c[1]-c[2]*c[2]
     if denom != 0:
-        ysh = limit_abs_one((-2*c[0]*c[4]+c[2]*c[3])/denom - 2.)
-        xsh = limit_abs_one((-2*c[1]*c[3]+c[2]*c[4])/denom - 2.)
+        ysh = _limit_abs_one((-2*c[0]*c[4]+c[2]*c[3])/denom - 2.)
+        xsh = _limit_abs_one((-2*c[1]*c[3]+c[2]*c[4])/denom - 2.)
 
     yt = peak_y + ysh
     xt = peak_x + xsh

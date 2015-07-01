6476785195ce0370a2226cda5599a11efe00ac48
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-01 22:00:48 +0900

    Update a correlation test code

--- xni/align/tests/test_correlation.py
+++ xni/align/tests/test_correlation.py
@@ -1,26 +1,26 @@
 import numpy as np
+from numpy.testing import assert_allclose
+
 from scipy.ndimage.interpolation import shift
 import matplotlib.pyplot as plt
 
 from xni.align import correlation
 
-np.set_printoptions(precision=3)
-
+ntry = 100
 
 def test_corr1d():
     A = np.array([1, 3, 5, 3, 1], dtype=np.double)
     A = np.pad(A, 10, mode='constant', constant_values=1)
-    err_count = 0
-    ntry = 1000
 
-    for _ in range(ntry):
-        rand = np.ceil(np.random.random()*50.-100.)/10.
-        B = shift(A, (rand), mode='constant', cval=1.0)
-        corr = correlation.corr1d(A, B)
-        err_count = err_count + (0 if np.fabs(rand-corr) < 0.1 else 1)
+    rand = np.zeros(ntry, dtype=np.double)
+    corr = np.zeros(ntry, dtype=np.double)
 
-    print ('----')
-    print (err_count)
+    for i in range(ntry):
+        rand[i] = np.ceil(np.random.random()*50.-100.)/10.
+        B = shift(A, (rand[i]), mode='constant', cval=1.0)
+        corr[i] = correlation.corr1d(A, B)
+
+    assert_allclose(rand, corr, rtol=0, atol=0.1)
 
 
 def test_corr2d():
@@ -32,19 +32,16 @@ def test_corr2d():
 
     A = np.pad(A, 10, mode='constant', constant_values=1)
 
-    err_x, err_y = 0, 0
-    ntry = 1000
+    rand_dx = np.zeros(ntry, dtype=np.double)
+    rand_dy = np.zeros(ntry, dtype=np.double)
+    corr_dx = np.zeros(ntry, dtype=np.double)
+    corr_dy = np.zeros(ntry, dtype=np.double)
 
-    for _ in range(ntry):
-        rand_dy = np.ceil(np.random.random()*50.-100.)/10.
-        rand_dx = np.ceil(np.random.random()*50.-100.)/10.
-        B = shift(A, (rand_dy, rand_dx), mode='constant', cval=1.0)
-        corr_dy, corr_dx = correlation.corr2d(A, B)
-        err_y = err_y + (0 if np.fabs(rand_dy-corr_dy) < 0.1 else 1)
-        err_x = err_x + (0 if np.fabs(rand_dx-corr_dx) < 0.1 else 1)
+    for i in range(ntry):
+        rand_dy[i] = np.ceil(np.random.random()*50.-100.)/10.
+        rand_dx[i] = np.ceil(np.random.random()*50.-100.)/10.
+        B = shift(A, (rand_dy[i], rand_dx[i]), mode='constant', cval=1.0)
+        corr_dy[i], corr_dx[i] = correlation.corr2d(A, B)
 
-    print ('----')
-    print (err_y, err_x)
-
-# ValueError
-#print (correlation.corr2d(A, C))
+    assert_allclose(rand_dx, corr_dx, rtol=0, atol=0.1)
+    assert_allclose(rand_dy, corr_dy, rtol=0, atol=0.1)

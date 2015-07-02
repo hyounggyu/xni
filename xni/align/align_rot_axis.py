01d5e85b37eb76343fe744db72b7b9c7a877d640
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-02 15:12:31 +0900

    Update align_rot_axis parallel code

--- xni/align/align_rot_axis.py
+++ xni/align/align_rot_axis.py
@@ -10,7 +10,11 @@ def align_rot_axis(data, axis=2, lview=None):
     '''
 
     d = np.sum(data, axis=axis)
-    map_obj = map(corr1d, d, d[1:])
+    if lview == None:
+        map_obj = map(corr1d, d, d[1:])
+    else:
+        lview.block = True
+        map_obj = lview.map(corr1d, d, d[1:])
     res = np.array(list(map_obj))
     res = np.insert(np.cumsum(res), 0, 0)
 

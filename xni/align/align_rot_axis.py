8783fb04eb3c8aea2c810a069d7286da877908e4
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-03 00:22:48 +0900

    Add block options in align_rot_axis

--- xni/align/align_rot_axis.py
+++ xni/align/align_rot_axis.py
@@ -3,7 +3,7 @@ import numpy as np
 from .correlation import corr1d
 
 
-def align_rot_axis(data, axis=2, lview=None):
+def align_rot_axis(data, axis=2, lview=None, block=True):
     '''
     data: 3d volume data
     axis: sum axis
@@ -13,7 +13,7 @@ def align_rot_axis(data, axis=2, lview=None):
     if lview == None:
         map_obj = map(corr1d, d, d[1:])
     else:
-        lview.block = True
+        lview.block = block
         map_obj = lview.map(corr1d, d, d[1:])
     res = np.array(list(map_obj))
     res = np.insert(np.cumsum(res), 0, 0)

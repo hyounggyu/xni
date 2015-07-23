9156238819aaa4135f28bc3e77482e21fef59116
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-23 22:27:04 +0900

    Add fromiter function

---
+++ xni/_shared/__init__.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def fromiter(iter_obj):
+    return np.array(list(iter_obj))
--- xni/align/_align.py
+++ xni/align/_align.py
@@ -1,19 +1,13 @@
 import numpy as np
 from scipy.ndimage.interpolation import shift as ndshift
 
-from .correlation import corr1d
+from ..._shared import fromiter
+from .corr import corr1d
 
 
-def shift(data, pos, lview=None):
-    if lview == None:
-        map_obj = map(ndshift, data, pos)
-    else:
-       lview.block = True
-       map_obj = lview.map(ndshift, data, pos)
-
-    res = np.array(list(map_obj))
-
-    return res
+def shift(data, pos, _map=map):
+    map_obj = _map(ndshift, data, pos)
+    return fromiter(map_obj)
 
 
 def align_rot_axis(data, axis=2, data_shift=False, lview=None):
@@ -22,14 +16,8 @@ def align_rot_axis(data, axis=2, data_shift=False, lview=None):
     axis: sum axis
     '''
     d = np.sum(data, axis=axis)
-
-    if lview == None:
-        map_obj = map(corr1d, d, d[1:])
-    else:
-        lview.block = True
-        map_obj = lview.map(corr1d, d, d[1:])
-
+    map_obj = _map(corr1d, d, d[1:])
     res = np.fromiter(map_obj, np.double)
-    res = np.insert(np.cumsum(res), 0, 0)
-
+    res = np.cumsum(res)
+    res = np.insert(res, 0, 0)
     return res

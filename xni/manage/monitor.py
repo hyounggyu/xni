c466ada917c0a432a3c961c359afe0c69ad900d6
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-27 16:34:51 +0900

    Update status.json

--- xni/manage/monitor.py
+++ xni/manage/monitor.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'hello'
-
+# [id: 1, cpu_persent: 10.0]
 @app.route('/status.json')
 def status():
+    cpu = ps.cpu_percent(1, percpu=True)
+    mem = ps.virtual_memory()
+    mem_dict = dict(mem.__dict__)
     response = jsonify(
-        cpu_count=ps.cpu_count(),
-        cpu_percent=ps.cpu_percent(1, percpu=True)
+        cpu_percents=[{'cpu_id': i, 'cpu_percent': load} for i, load in enumerate(cpu)],
+        mem=mem_dict
         )
     return response
 
 def start_monitor(args):
     # TODO: check debug mode
-    app.run(debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, use_reloader=True)

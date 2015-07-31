7408ce58282e92c968d7773c6914ac5f911486c0
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-31 17:17:10 +0900

    Remove shift, Rename rot_axis to vlign, Add center of rotation

--- xni/adjust/align.py
+++ xni/adjust/align.py
@@ -1,23 +1,25 @@
 import numpy as np
-from scipy.ndimage.interpolation import shift as ndshift
 
-from ..util import fromiter
-from ..calc.corr import corr1d
+from ..calc.corr import corr1d, corr2d
 
 
-def shift(data, pos, _map=map):
-    map_obj = _map(ndshift, data, pos)
-    return fromiter(map_obj)
-
-
-def rot_axis(data, axis=2, data_shift=False, _map=map):
+def valign(data, _map=map):
     '''
-    data: 3d volume data
-    axis: sum axis
+    data: 3d volume data (0:2pi, top:bottom, left:right)
     '''
-    d = np.sum(data, axis=axis)
+    d = np.sum(data, axis=2) # axis 2 is left-right
     map_obj = _map(corr1d, d, d[1:])
     res = np.fromiter(map_obj, np.double)
     res = np.cumsum(res)
-    res = np.insert(res, 0, 0)
+    res = -1.0 * np.insert(res, 0, 0)
     return res
+
+
+def rot_center(data):
+    '''
+    data: 0...2pi
+    return
+    move position
+    '''
+    yt, xt = corr2d(data[0], np.fliplr(data[-1]))
+    return xt
--- xni/util/__init__.py
+++ xni/util/__init__.py
@@ -1,4 +1,9 @@
+import collections
+
 import numpy as np
 
 def fromiter(iter_obj):
     return np.array(list(iter_obj))
+
+def isvector(val):
+    return isinstance(val, (collections.Sequence, np.ndarray))

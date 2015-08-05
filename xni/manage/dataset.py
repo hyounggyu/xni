cc554625d2c28b841103353d4135ba099a45ebe3
Hyounggyu Kim <hgkim10@gmail.com>
2015-08-05 10:17:22 +0900

    Separate dataset

--- xni/manage/dataset.py
+++ xni/manage/dataset.py
@@ -1,41 +1,11 @@
 from pathlib import Path
-import socket
-import struct
-import pickle
 
-import h5py
-import numpy as np
-from skimage.external.tifffile import imread
-import zmq
+from ..dataset.io import create
 
-
-def create(output, images, bgnds=[], darks=[],
-    groupname='original', images_dsetname='images',
-    bgnds_dsetname='bgnds', darks_dsetname='darks', dtype='i2', generator=True):
-
-    fd = h5py.File(output, 'w')
-    grp = fd.create_group(groupname)
-
-    ny, nx = imread(images[0]).shape # All images are same shape
-    if len(bgnds) > 0:
-        bgnds_dset = grp.create_dataset(bgnds_dsetname, (len(bgnds), ny, nx), dtype=dtype)
-    if len(darks) > 0:
-        darks_dset = grp.create_dataset(darks_dsetname, (len(darks), ny, nx), dtype=dtype)
-    images_dset = grp.create_dataset(images_dsetname, (len(images), ny, nx), dtype=dtype)
-
-    for i, im in enumerate(bgnds):
-        bgnds_dset[i,:,:] = imread(im)[:,:]
-
-    for i, im in enumerate(darks):
-        darks_dset[i,:,:] = imread(im)[:,:]
-
-    for i, im in enumerate(images):
-        images_dset[i,:,:] = imread(im)[:,:]
-        if generator:
-            yield i, im
 
 def _findtiff(path, prefix):
     return sorted([p for p in path.iterdir() if p.match(prefix.strip()+'*') and (p.suffix.lower() in ['.tif', '.tiff'])])
+
 
 def start_create(args):
     path = Path(args.path)
@@ -48,85 +18,3 @@ def start_create(args):
     darks = [str(im) for im in darks]
     for i, name in create(args.output, images, bgnds, darks):
         print(i, name)
-
-def load(filename, grp='original', dset='images', dtype='f4'):
-    '''
-    default datatype is 4byte float
-    '''
-    with h5py.File(filename, 'r') as f:
-        dset = f[grp][dset]
-        with dset.astype(dtype):
-            out = dset[:]
-    return out
-
-def send(*dsets, ip='127.0.0.1', port=5051):
-    context = zmq.Context()
-
-    with context.socket(zmq.REP) as socket:
-        socket.bind('tcp://{}:{}'.format(ip, port))
-        index = socket.recv_pyobj()
-        for dset in dsets[:-1]:
-            socket.send_pyobj(dset[index], flags=zmq.SNDMORE)
-        socket.send_pyobj(dsets[-1][index])
-        socket.recv() # wait bye message
-
-
-def recv(index=None, ip='127.0.0.1', port=5051, timeout=10):
-    '''
-    timeout = unit sec
-    '''
-    if index == None:
-        index = np.index_exp[0:1]
-
-    with zmq.Context() as context:
-        with context.socket(zmq.REQ) as socket:
-            # https://github.com/zeromq/pyzmq/issues/132
-            socket.setsockopt(zmq.LINGER, 0)
-            socket.connect('tcp://{}:{}'.format(ip, port))
-            socket.send_pyobj(index)
-            poller = zmq.Poller()
-            poller.register(socket, zmq.POLLIN)
-            if poller.poll(timeout*1000):
-                parts = [socket.recv_pyobj()]
-            else:
-                raise IOError('Timeout reciving data')
-            while socket.getsockopt(zmq.RCVMORE):
-                part = socket.recv_pyobj()
-                parts.append(part)
-            socket.send(b'BYE')
-    return parts
-
-
-def serve(data, host='', port=5051):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    s.listen(1)
-    conn, addr = s.accept()
-    print('Connected by', addr)
-    r = conn.recv(1024)
-    if not r:
-        raise RuntimeError("socket connection broken")
-    msg = data.dumps()
-    length = len(msg)
-    conn.send(struct.pack('!Q', length))
-    conn.sendall(msg)
-    conn.close()
-
-
-def get(index=(0, None, 1), host='127.0.0.1', port=5051):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    print('send hello')
-    s.sendall(b'Hello')
-    length = s.recv(struct.calcsize('!Q'))
-    length, = struct.unpack('!Q', length)
-    data = b''
-    recvsize = 0
-    while recvsize < length:
-        chunk = s.recv(1024)
-        if chunk == b'':
-            raise RuntimeError("socket connection broken")
-        recvsize += len(chunk)
-        data += chunk
-    s.close()
-    return pickle.loads(data)

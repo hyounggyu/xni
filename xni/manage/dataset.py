f378f3265e93b6cfbd96f1f9e6e771db2386f2f1
Hyounggyu Kim <hgkim10@gmail.com>
2015-08-05 10:01:47 +0900

    Add dataset.{serve,get}

--- xni/manage/dataset.py
+++ xni/manage/dataset.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import socket
+import struct
+import pickle
 
 import h5py
 import numpy as np
@@ -92,3 +95,38 @@ def recv(index=None, ip='127.0.0.1', port=5051, timeout=10):
                 parts.append(part)
             socket.send(b'BYE')
     return parts
+
+
+def serve(data, host='', port=5051):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    r = conn.recv(1024)
+    if not r:
+        raise RuntimeError("socket connection broken")
+    msg = data.dumps()
+    length = len(msg)
+    conn.send(struct.pack('!Q', length))
+    conn.sendall(msg)
+    conn.close()
+
+
+def get(index=(0, None, 1), host='127.0.0.1', port=5051):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    print('send hello')
+    s.sendall(b'Hello')
+    length = s.recv(struct.calcsize('!Q'))
+    length, = struct.unpack('!Q', length)
+    data = b''
+    recvsize = 0
+    while recvsize < length:
+        chunk = s.recv(1024)
+        if chunk == b'':
+            raise RuntimeError("socket connection broken")
+        recvsize += len(chunk)
+        data += chunk
+    s.close()
+    return pickle.loads(data)

7295b578b9661dd34aeeb2cabe239b8553299afa
Hyounggyu Kim <hgkim10@gmail.com>
2015-08-20 12:11:25 +0900

    Change protocol

--- xni/dataset/comm.py
+++ xni/dataset/comm.py
@@ -4,6 +4,31 @@ import pickle
 
 import numpy as np
 
+# First byte is reserved
+# Java doen't have unsigned type
+DTYPE = '>f4'
+ARRINFOTYPE = '!ciii'
+
+
+def getheader(arr):
+    if len(arr.shape) == 2:
+        shape = (1,) + arr.shape
+    elif len(arr.shape) == 3:
+        shape = arr.shape
+    else:
+        raise TypeError('2d or 3d array only')
+    return struct.pack(ARRINFOTYPE, b'\x00', *shape)
+
+
+def getarrinfo(msg):
+    tmp, *shape = struct.unpack(ARRINFOTYPE, msg)
+    # Receive only 32bit float type
+    dtype = DTYPE
+    size = 4
+    count = shape[0]*shape[1]*shape[2]
+    length = count * size
+    return dtype, shape, count, length
+
 
 def serve(data, host='', port=5051):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,29 +42,18 @@ def serve(data, host='', port=5051):
         return
 
     print('Connected by {}'.format(addr))
-    r = conn.recv(struct.calcsize('!3i')) # Receive index
+    r = conn.recv(1) # Wait client
     if not r: raise RuntimeError("socket connection broken")
-    start, end, step = struct.unpack('!3i', r) # Unpacking three integers
-    # end, step values cannot be zero
-    end = None if end == 0 else end
-    step = None if step == 0 else step
-    msg = data[start:end:step].dumps()
-    conn.send(struct.pack('!Q', len(msg)))
-    conn.sendall(msg)
+    conn.send(getheader(data))
+    conn.sendall(data.astype(DTYPE).tobytes()) # Big endian 32bit float
 
 
-def get(index=(0, None, 1), host='127.0.0.1', port=5051):
-    '''
-    index = (start, end, step)
-    '''
+def get(host='127.0.0.1', port=5051):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    # If end and/or step are None, it will be changed zero.
-    # zero end/step are unpacked None
-    idx = tuple(map(lambda v: int(v or 0), index))
-    s.sendall(struct.pack('!3i', *idx)) # Packing three integers
-    length = s.recv(struct.calcsize('!Q')) # Receive data length
-    length, = struct.unpack('!Q', length)
+    s.sendall(b'\x00') # Send hello
+    msg = s.recv(struct.calcsize(ARRINFOTYPE)) # Receive data length
+    dtype, shape, count, length = getarrinfo(msg)
     data = []
     recvsize = 0
     while recvsize < length:
@@ -49,4 +63,4 @@ def get(index=(0, None, 1), host='127.0.0.1', port=5051):
         recvsize += len(chunk)
         data.append(chunk)
     s.close()
-    return pickle.loads(b''.join(data))
+    return np.frombuffer(b''.join(data), dtype=dtype, count=count, offset=0).reshape(shape)
